a47661d5b2e51c4047d45504bdf65fdff6fd8b9e
lirco <lirco77@gmail.com>
2014-11-08 16:05:41 +0200

    initial commit

---
+++ app/models/product.server.model.js
@@ -0,0 +1,91 @@
+'use strict';
+
+/**
+ * Module dependencies.
+ */
+var mongoose = require('mongoose'),
+	Schema = mongoose.Schema;
+
+/**
+ * Product Schema
+ */
+var ProductSchema = new Schema({
+	title: {
+		type: String,
+		default: '',
+		required: 'Please fill product title',
+		trim: true
+	},
+  shortDescription: {
+    type: String,
+    default: '',
+    required: 'Please fill product short description',
+    trim: true
+  },
+  fullDescription: {
+    type: String,
+    default: '',
+    required: 'Please fill product full description',
+    trim: true
+  },
+  colors: {
+    type: Array,
+    default: '',
+    required: 'Please fill product colors',
+    trim: true
+  },
+  sizes: {
+    type: Array,
+    default: '',
+    required: 'Please fill product sizes',
+    trim: true
+  },
+  price: {
+    type: Number,
+    default: '',
+    required: 'Please fill product price (in $US)',
+    trim: true
+  },
+  mainImage: {
+    type: string,
+    default: '',
+    required: 'Please insert product main image',
+    trim: true
+  },
+  images: {
+    type: Array,
+    default: '',
+    required: 'Please insert product images',
+    trim: true
+  },
+  categories: {
+    type: Array,
+    default: '',
+    required: 'Please insert product categories',
+    trim: true
+  },
+  views: {
+    type: Number,
+    default: '',
+    trim: true
+  },
+  purchased: {
+    type: Number,
+    default: '',
+    trim: true
+  },
+	created: {
+		type: Date,
+		default: Date.now
+	},
+  modified: {
+    type: Date,
+    default: Date.now
+  },
+	user: {
+		type: Schema.ObjectId,
+		ref: 'User'
+	}
+});
+
+mongoose.model('Product', ProductSchema);
